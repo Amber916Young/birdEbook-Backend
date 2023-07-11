94539b3d564f0b83cf66dfc547e45565d2ba48c4
hupochuanbird <hupochuan007@gmail.com>
2023-07-11 22:27:45 +0100

    nothing

---
+++ src/main/java/com/bird/app/controller/TagsController.java
@@ -0,0 +1,21 @@
+package com.bird.app.controller;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author birdyyoung
+ * @ClassName:WebCategoryController
+ * @Description:
+ * @Date: 10/07/2023 14:46
+ * @Version: v1.0
+ */
+@RestController
+@RequestMapping("/api/tags")
+@RequiredArgsConstructor
+@Slf4j
+public class TagsController {
+
+}
