56abf4fc6d5f4a0cab235b2d8abfb869b8e89d1a
birdflytomoon <yejia.yang@mycloudpa.com>
2023-07-20 10:29:59 +0100

    define the new structure of article: test successfully

--- src/main/java/com/bird/common/entity/Article.java
+++ src/main/java/com/bird/common/entity/Article.java
@@ -35,7 +35,7 @@ public class Article{
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column
-    private long id;
+    private Long id;
 
     @NotNull
     @Size(min = 1, max = 100)
