6655cb50494d621c5153f446227b56ac820325f9
birdflytomoon <yejia.yang@mycloudpa.com>
2023-08-02 10:02:38 +0100

    add sql file

--- src/main/java/com/bird/app/controller/TagsController.java
+++ src/main/java/com/bird/app/controller/TagsController.java
@@ -56,7 +56,7 @@ public class TagsController {
     /***
      *
      * TODO 当删除标签的时候，检查taguselog（中间表）是否存在这个id，如果存在，有两种情况
-     * 1）与之关联的文章只有这一个标签 taguselog的tagid指向 "其他" 标签 （这个其他标签是不可删除，定义为id=0的）
+     * 1）与之关联的文章只有这一个标签 taguselog的tagid指向 "其他" 标签 （这个其他标签是不可删除，定义为id=1的）
      * 2）与之关联的文章有多个标签 直接删除记录  tag/taguselog 两个表
      * @param id
      * @return
