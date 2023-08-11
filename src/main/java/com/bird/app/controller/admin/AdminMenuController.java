caef8fc3a098ba6fa947130c540692e6347c0280
birdflytomoon <yejia.yang@mycloudpa.com>
2023-08-11 17:18:37 +0100

    new feature

--- src/main/java/com/bird/app/controller/admin/AdminMenuController.java
+++ src/main/java/com/bird/app/controller/admin/AdminMenuController.java
@@ -2,6 +2,9 @@ package com.bird.app.controller.admin;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,4 +13,9 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 @Slf4j
 public class AdminMenuController {
+    private final AdminMenuService adminMenuService;
+    @GetMapping(produces = "application/json")
+    public ResponseEntity<?> getAllMenuTree() {
+        return new ResponseEntity<>(adminMenuService.findAllMenu(), HttpStatus.OK);
+    }
 }
